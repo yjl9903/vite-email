f00bb59f033e617f29ba3f4e4499b0bd7bd8f2d9
XLor <yjl9903@vip.qq.com>
2022-03-24 03:30:43 +0800

    chore: move

--- example/vite.config.ts
+++ example/vite.config.ts
@@ -3,9 +3,9 @@ import { defineConfig } from 'vite-plugin-email';
 export default defineConfig({
   email: {
     host: 'smtp.yeah.net',
+    secure: true,
     auth: {
       user: 'yan_jl@yeah.net'
-    },
-    secure: true
+    }
   }
 });
