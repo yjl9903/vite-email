b6e4f6cc672306563da10fd7a84c315aa0df9b41
XLor <yjl9903@vip.qq.com>
2022-03-24 22:56:50 +0800

    fix: border color

--- client/src/App.vue
+++ client/src/App.vue
@@ -46,7 +46,7 @@ watch(
     </div>
   </nav>
   <div id="main" class="font-none">
-    <div id="sidebar" class="h-full border-0 border-r border-gray-400 min-w-48 overflow-auto">
+    <div id="sidebar" class="h-full border-0 border-r min-w-48 overflow-auto">
       <div v-for="r in receivers" @click="preview = r.receiver">{{ r.receiver }}</div>
     </div>
     <div
@@ -79,7 +79,8 @@ a:active {
 }
 
 nav {
-  @apply px-6 border-0 border-b border-gray-400 gap-4 h-54px children:my-auto;
+  @apply px-6 border-0 border-b gap-4 h-54px children:my-auto;
+  border-color: rgba(156,163,175, 0.3);
 }
 
 .btn {
@@ -105,9 +106,14 @@ nav {
   font-weight: lighter;
 }
 
+#sidebar {
+  border-color: rgba(156,163,175, 0.3);
+}
+
 #sidebar > div {
   @apply block border-0 border-b px-6 py-2 text-left font-mono text-sm;
   @apply cursor-pointer underline-transparent;
   @apply hover:bg-light-400;
+  border-color: rgba(156,163,175, 0.3);
 }
 </style>
