13509bd0299cbd5c0eff5ce0d1bf08c3be4f49f4
XLor <yjl9903@vip.qq.com>
2022-03-24 14:31:42 +0800

    chore: format

--- build.config.ts
+++ build.config.ts
@@ -5,9 +5,7 @@ export default defineBuildConfig({
   entries: ['src/index', 'src/cli'],
   declaration: true,
   clean: true,
-  externals: [
-    path.join(__dirname, './package.json')
-  ],
+  externals: [path.join(__dirname, './package.json')],
   rollup: {
     emitCJS: true
   }
